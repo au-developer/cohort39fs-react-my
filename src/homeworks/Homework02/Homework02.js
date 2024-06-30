73f112df9cfcab35b5d9ac054dfa6024b680fbd7
au-developer <git2024git@gmail.com>
2024-06-30 20:56:57 +0300

    first commit homework02

---
+++ src/homeworks/Homework02/Homework02.js
@@ -0,0 +1,13 @@
+import Card from "../../components/Card/Card";
+import "./styles.css";
+function Homework02() {
+  return (
+    <div className="hw02-component">
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
+
+export default Homework02;
