eddce6648fb6c7f84cfd32c08b1ea15a662ed053
au-developer <git2024git@gmail.com>
2024-07-01 18:06:41 +0300

    add lesson03

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 // Lessons
 //import Lesson02 from "./lessons/lesson02/Lesson02";
+import Lesson03 from "./lessons/lesson03/Lesson03"
 
 // Homeworks
 
 // Consultations
-import Consultation01 from "./consultations/Consultation01/Consultation01";
+//import Consultation01 from "./consultations/Consultation01/Consultation01";
 
 function App() {
   return (
     <div className="App">
       {/* <Lesson02 /> */}
-      <Consultation01/>
+      {/* <Consultation01/> */}
+      <Lesson03/>
     </div>
   );
 }
