532bf5ecb8aa5a0ddc3ac67a22bdef8d9a15044b
au-developer <git2024git@gmail.com>
2024-07-02 10:17:39 +0300

    add teacher advice

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,16 +2,16 @@ import "./styles.css";
 
 function Card() {
   const homer = {
-    photo: (
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE7n03AXMn7LXZSl4G3g2PHlAoo82AtPXCfg&s" />
-    ),
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE7n03AXMn7LXZSl4G3g2PHlAoo82AtPXCfg&s",
+
     name: "Homer Simpson",
     work: "Drink and Sleep",
     hobby: "Troubleshooting",
   };
   return (
     <div className="card-component">
-      <p>{homer.photo}</p>
+      <img src={homer.photo} />
       <p>Name: {homer.name}</p>
       <p>Work: {homer.work}</p>
       <p>Hobby: {homer.hobby}</p>
