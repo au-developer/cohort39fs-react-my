53a8ab779102371ade301c13179e502b8199177e
au-developer <git2024git@gmail.com>
2024-07-03 19:48:16 +0300

    lesson05

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import "./App.css";
 // Lessons
 //import Lesson02 from "./lessons/lesson02/Lesson02";
 //import Lesson03 from "./lessons/lesson03/Lesson03"
-import Lesson04 from "./lessons/lesson04/Lesson04"
+//import Lesson04 from "./lessons/lesson04/Lesson04"
+import Lesson05 from "./lessons/lesson05/Lesson05"
 
 // Homeworks
 // import Homework02 from "./homeworks/Homework02/Homework02";
 //import Homework02t from "./homeworks/Homework02t/Homework02t";
-import Homework03t from "./homeworks/Homework03t/Homework03t";
+//import Homework03t from "./homeworks/Homework03t/Homework03t";
 // Consultations
 // import Consultation01 from "./consultations/Consultation01/Consultation01";
 
@@ -20,7 +21,8 @@ function App() {
       {/* <Homework02t/> */}
       {/* <Lesson03/> */}
       {/* <Homework03t/> */}
-      <Lesson04/>
+       {/* <Lesson04/> */}
+       <Lesson05/>
     </div>
   );
 }
